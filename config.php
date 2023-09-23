f438c4c82c5a4e9f8e7b05e15997c362e04bd9ed
RafiqSaputra18 <144555814+RafiqSaputra18@users.noreply.github.com>
2023-09-23 05:40:34 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+   $server = "localhost";
+   $username = "root";
+   $password = "";
+   $database = "pmb_hanarohaena_uas";
+
+   $conn = mysqli_connect($server,$username,$password,$database);
+
+?>
