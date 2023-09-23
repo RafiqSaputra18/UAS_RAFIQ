f6e7362d019edb45ed7275df438adb05d6066786
RafiqSaputra18 <144555814+RafiqSaputra18@users.noreply.github.com>
2023-09-23 06:04:11 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penerimaan Mahasiswa Baru STMIK AL-FATH</title>
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Sertakan Bootstrap CSS dari CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Penerimaan Mahasiswa Baru STMIK ALFATH 2023</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="form-daftar.php">Pendaftaran</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="list-mahasiswa.php">Pendaftar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Hero Section -->
+    <div class="container mt-5">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome To STMIK ALFATH</h1>
+            <p class="lead">Kami menyambut Anda untuk bergabung dengan Kampus kami. Daftarkan diri Anda segera!</p>
+            <hr class="my-4">
+            <p>Dapatkan Gratis SPP dan Biaya Pembangunan untuk 100 Orang Pertama!</p>
+            <a class="btn btn-primary btn-lg" href="form-daftar.php" role="button">Daftar Sekarang</a>
+        </div>
+    </div>
+
+    <!-- Sertakan Bootstrap JavaScript dari CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
